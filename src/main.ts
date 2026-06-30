8b78efb03461cc6bf5e4f96afb0904b8bd891bf9
llooht <tonix2929@gmail.com>
2026-06-30 21:33:57 +0200

    Okta hinzugefügt

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import OktaVue from '@okta/okta-vue'
-import { oktaAuth } from './okta'
+import { oktaAuth } from './Okta'
 
 const app = createApp(App)
 
